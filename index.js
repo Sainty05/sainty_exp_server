335c434b8f3404844dafd6a10ef0d176be24497a
Santosh Rathore <santosh.rathore@infraveo.com>
2023-08-11 14:42:41 +0530

    update cors

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const app = express();
 const port = 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://sainty-exp-app-05.vercel.app',
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // serve up production assets
@@ -35,7 +41,7 @@ app.get('/', (req, res) => {
 
 app.options('/api', cors()); // Respond to preflight requests
 // Use API routes
-app.use('/api', router );
+app.use('/api', router);
 
 // Start the server
 app.listen(port, () => {

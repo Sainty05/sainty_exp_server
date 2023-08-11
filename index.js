ea2ac03e0defbb0f05e1367b1c80d5a827e87878
Santosh Rathore <santosh.rathore@infraveo.com>
2023-08-11 13:10:05 +0530

    update cors setting

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ app.use(express.static('./client/build'));
 app.use(cors());
 app.use(express.json());
 
+// Configure CORS to allow requests from your frontend domain
+app.use(cors({
+  origin: 'https://sainty-exp-app-05.vercel.app',
+  methods: ['GET', 'POST'],  // Adjust the allowed methods as needed
+  credentials: true,         // Allow cookies, authentication headers, etc.
+}));
+
 // Connect to MongoDB (using the cloud-based MongoDB connection URL)
 const dbURI = process.env.MONGO_DB_URL
 
@@ -43,32 +50,32 @@ mongoose.connect(dbURI, {
     console.error('Error connecting to MongoDB:', err.message);
   });
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-  app.post('/login', async (req, res) => {
-    try {
-      const user = await User.findOne({ email: req.body.email });
-      let sessionUserId = user._id
-      let sessionToken = crypto.randomBytes(16).toString("hex");
-      if (!user) {
-        return res.status(400).json({ message: 'Invalid credentials' });
-      } else {
-        const passwordMatch = await bcrypt.compare(req.body.password, user.password);
-        if (!passwordMatch) {
-          return res.status(400).json({ message: 'Invalid password' });
-        }
-        const newToken = new Session({ userId: sessionUserId, sessionToken: sessionToken })
-        await newToken.save();
-  
-        res.json({ message: 'User logged in successfully', session: { sessionToken: sessionToken, sessionId: newToken._id, sessionUserId: sessionUserId, sessionUserName: user.userName } });
-        // res.json({ message: 'User logged in successfully', user });
+app.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    let sessionUserId = user._id
+    let sessionToken = crypto.randomBytes(16).toString("hex");
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    } else {
+      const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+      if (!passwordMatch) {
+        return res.status(400).json({ message: 'Invalid password' });
       }
-    } catch (err) {
-      res.status(500).json({ message: err.message });
+      const newToken = new Session({ userId: sessionUserId, sessionToken: sessionToken })
+      await newToken.save();
+
+      res.json({ message: 'User logged in successfully', session: { sessionToken: sessionToken, sessionId: newToken._id, sessionUserId: sessionUserId, sessionUserName: user.userName } });
+      // res.json({ message: 'User logged in successfully', user });
     }
-  });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // Use API routes
 // app.use('/api', require('./routes/api'));
